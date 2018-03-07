ba5c5da3730d6391cd7aa159c3139b2b0ffde16b
Paul Kincaid <paul.m.kincaid@gmail.com>
2018-03-06 21:48:01 -0700

    Initial Push

---
+++ lib/listener.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/upload', methods= ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['files']
+        f.save('/Users/paul/bin/__Github_Enabled__/ir4n6/email_parser/newTest.txt')
+        return '200'
+    else:
+        return 'Upload Page'
+
+#f = 'original_msg.txt'
+#fl = {'files': open(f, 'rb')}
+#r = requests.post('http://127.0.0.1:5000/upload', files=fl)
